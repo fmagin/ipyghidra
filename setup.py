77357e9ef5b9a4e6c9076c0074352a45051995ca
Florian Magin <fmagin@users.noreply.github.com>
2019-12-15 19:31:22 +0100

    Handle cattrs being broken in Python 3.8

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from distutils.core import setup
 
+import sys
+
 setup(
     name="ipyghidra",
     version="1.0",
@@ -7,5 +9,5 @@ setup(
     author="Florian Magin",
     url="none",
     packages=["ipyghidra"],
-    install_requires=["ghidra_bridge", "ipython"],
+    install_requires=["ghidra_bridge", "ipython", "attr", "cattrs-3.8" if sys.version_info >= (3, 8) else "cattrs"],
 )
